24eaa3784f65b310fe6700e33860489e2327a7f1
Angelia Beek <72133720+angeliabeek@users.noreply.github.com>
2021-01-09 14:03:46 +0100

    Add files via upload

---
+++ Opdr4_gebruik_ifelse_statements/script.js
@@ -0,0 +1,26 @@
+// Access to bar based on age
+const customersAge = 30;
+if (customersAge >= 18) {
+    console.log("You may enter the bar");
+}
+else {
+    console.log("You can't enter the bar");
+}
+
+// Ladiesnight access only for ladies
+const isFemale = true;
+if (isFemale == true) {
+    console.log("Welcome to ladiesnight");
+}
+else {
+    console.log("Men aren't welcome to ladiesnight");
+}
+
+// Only bob is able to drive home
+const driverStatus = 'bob';
+if (driverStatus == 'bob') {
+    console.log("You can drive home");
+}
+else {
+    console.log("You can't drive home");
+}
